8d4cd122b830caf92756662580cd8b1e3897a55d
Suraj Sharma <sharmasurajkumar4649@gmail.com>
2026-05-27 16:32:18 +0530

    OOPs

---
+++ src/demo/demo2Test.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class demo2Test {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module System_design_Concepts {
+}
